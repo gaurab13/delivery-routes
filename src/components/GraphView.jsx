6ef668195f3db24925e264ec4ef13a0c0da94ab1
Gaurav KC <13gaurab@gmail.com>
2020-09-11 10:05:57 +0545

    Add bootstrap and format using prettier

--- src/components/GraphView.jsx
+++ src/components/GraphView.jsx
@@ -1,40 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { GraphView } from 'react-digraph';
 
-const GraphConfig =  {
+const GraphConfig = {
   NodeTypes: {
-    empty: { // required to show empty nodes
-      typeText: "Town",
-      shapeId: "#empty", // relates to the type property of a node
+    empty: {
+      // required to show empty nodes
+      typeText: 'Town',
+      shapeId: '#empty', // relates to the type property of a node
       shape: (
         <symbol viewBox="0 0 100 100" id="empty" key="0">
-          <circle cx="50" cy="50" r="20"></circle>
+          <circle cx="50" cy="50" r="16"></circle>
         </symbol>
-      )
+      ),
     },
-    custom: { // required to show empty nodes
-      typeText: "Custom",
-      shapeId: "#custom", // relates to the type property of a node
+    custom: {
+      // required to show empty nodes
+      typeText: 'Custom',
+      shapeId: '#custom', // relates to the type property of a node
       shape: (
         <symbol viewBox="0 0 50 25" id="custom" key="0">
           <ellipse cx="50" cy="25" rx="50" ry="25"></ellipse>
         </symbol>
-      )
-    }
+      ),
+    },
   },
   NodeSubtypes: {},
   EdgeTypes: {
-    emptyEdge: {  // required to show empty edges
-      shapeId: "#emptyEdge",
+    emptyEdge: {
+      // required to show empty edges
+      shapeId: '#emptyEdge',
       shape: (
         <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
-          <circle cx="25" cy="25" r="8" fill="currentColor"> </circle>
+          <circle cx="25" cy="25" r="10" fill="currentColor">
+            {' '}
+          </circle>
         </symbol>
-      )
-    }
-  }
-}
-const NODE_KEY = "id"; // Allows D3 to correctly update DOM
+      ),
+    },
+  },
+};
+const NODE_KEY = 'id'; // Allows D3 to correctly update DOM
 
 export default class Graph extends Component {
   constructor(props) {
@@ -57,99 +62,97 @@ export default class Graph extends Component {
     const EdgeTypes = GraphConfig.EdgeTypes;
 
     return (
-      <div id="graph">
-        <GraphView
-          nodeKey={NODE_KEY}
-          nodes={nodes}
-          edges={edges}
-          selected={selected}
-          nodeTypes={NodeTypes}
-          nodeSubtypes={NodeSubtypes}
-          edgeTypes={EdgeTypes}
-          onSelectNode={this.onSelectNode}
-          onUpdateNode={this.onUpdateNode}
-          onSelectEdge={this.onSelectEdge}
-          onCreateNode={this.onCreateNode}
-          onDeleteNode={this.onDeleteNode}
-          onSwapEdge={this.onSwapEdge}
-        />
-      </div>
+      <GraphView
+        nodeKey={NODE_KEY}
+        nodes={nodes}
+        edges={edges}
+        selected={selected}
+        nodeTypes={NodeTypes}
+        nodeSubtypes={NodeSubtypes}
+        edgeTypes={EdgeTypes}
+        onSelectNode={this.onSelectNode}
+        onUpdateNode={this.onUpdateNode}
+        onSelectEdge={this.onSelectEdge}
+        onCreateNode={this.onCreateNode}
+        onDeleteNode={this.onDeleteNode}
+        onSwapEdge={this.onSwapEdge}
+      />
     );
   }
 }
 
 const sample = {
-  "nodes": [
+  nodes: [
     {
-      "id": 1,
-      "title": "A",
-      "x": 0,
-      "y": 100,
-      "type": "empty"
+      id: 1,
+      title: 'A',
+      x: 0,
+      y: 100,
+      type: 'empty',
     },
     {
-      "id": 2,
-      "title": "B",
-      "x": 200,
-      "y": 100,
-      "type": "empty",
+      id: 2,
+      title: 'B',
+      x: 200,
+      y: 100,
+      type: 'empty',
     },
     {
-      "id": 3,
-      "title": "C",
-      "x": 200,
-      "y": 100,
-      "type": "empty"
+      id: 3,
+      title: 'C',
+      x: 200,
+      y: 100,
+      type: 'empty',
     },
     {
-      "id": 4,
-      "title": "D",
-      "x": 300,
-      "y": 200,
-      "type": "empty",
+      id: 4,
+      title: 'D',
+      x: 300,
+      y: 200,
+      type: 'empty',
     },
     {
-      "id": 5,
-      "title": "E",
-      "x": 300,
-      "y": 300,
-      "type": "empty"
+      id: 5,
+      title: 'E',
+      x: 300,
+      y: 300,
+      type: 'empty',
     },
     {
-      "id": 6,
-      "title": "F",
-      "x": 400,
-      "y": 300,
-      "type": "empty",
+      id: 6,
+      title: 'F',
+      x: 400,
+      y: 300,
+      type: 'empty',
     },
     {
-      "id": 7,
-      "title": "G",
-      "x": 400,
-      "y": 400,
-      "type": "empty",
+      id: 7,
+      title: 'G',
+      x: 400,
+      y: 400,
+      type: 'empty',
     },
     {
-      "id": 8,
-      "title": "H",
-      "x": 500,
-      "y": 400,
-      "type": "empty",
+      id: 8,
+      title: 'H',
+      x: 500,
+      y: 400,
+      type: 'empty',
     },
     {
-      "id": 9,
-      "title": "I",
-      "x": 500,
-      "y": 500,
-      "type": "empty",
+      id: 9,
+      title: 'I',
+      x: 500,
+      y: 500,
+      type: 'empty',
     },
   ],
-  "edges": [
+  edges: [
     {
-      "source": 1,
-      "target": 2,
-      "type": "edgeWithWeight",
-      "handleText": 5
+      source: 1,
+      target: 2,
+      type: 'edgeWithWeight',
+      handleText: 5,
     },
-  ]
-}
+  ],
+};
