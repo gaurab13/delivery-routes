3796869dd9b64ee13be3f834dc1dff0b3a5a7604
Gaurav KC <13gaurab@gmail.com>
2020-09-11 09:40:34 +0545

    Integrate react-digraph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GraphView from './components/GraphView';
 
 function App() {
   return (
     <div className="App">
-      Learn React
+      <GraphView />
     </div>
   );
 }
---
+++ src/components/GraphView.jsx
@@ -0,0 +1,155 @@
+import React, {Component} from 'react';
+import { GraphView } from 'react-digraph';
+
+const GraphConfig =  {
+  NodeTypes: {
+    empty: { // required to show empty nodes
+      typeText: "Town",
+      shapeId: "#empty", // relates to the type property of a node
+      shape: (
+        <symbol viewBox="0 0 100 100" id="empty" key="0">
+          <circle cx="50" cy="50" r="20"></circle>
+        </symbol>
+      )
+    },
+    custom: { // required to show empty nodes
+      typeText: "Custom",
+      shapeId: "#custom", // relates to the type property of a node
+      shape: (
+        <symbol viewBox="0 0 50 25" id="custom" key="0">
+          <ellipse cx="50" cy="25" rx="50" ry="25"></ellipse>
+        </symbol>
+      )
+    }
+  },
+  NodeSubtypes: {},
+  EdgeTypes: {
+    emptyEdge: {  // required to show empty edges
+      shapeId: "#emptyEdge",
+      shape: (
+        <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
+          <circle cx="25" cy="25" r="8" fill="currentColor"> </circle>
+        </symbol>
+      )
+    }
+  }
+}
+const NODE_KEY = "id"; // Allows D3 to correctly update DOM
+
+export default class Graph extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      graph: sample,
+      selected: {},
+    };
+  }
+  /* Define custom graph editing methods here */
+
+  render() {
+    const nodes = sample.nodes;
+    const edges = sample.edges;
+    const selected = this.state.selected;
+
+    const NodeTypes = GraphConfig.NodeTypes;
+    const NodeSubtypes = GraphConfig.NodeSubtypes;
+    const EdgeTypes = GraphConfig.EdgeTypes;
+
+    return (
+      <div id="graph">
+        <GraphView
+          nodeKey={NODE_KEY}
+          nodes={nodes}
+          edges={edges}
+          selected={selected}
+          nodeTypes={NodeTypes}
+          nodeSubtypes={NodeSubtypes}
+          edgeTypes={EdgeTypes}
+          onSelectNode={this.onSelectNode}
+          onUpdateNode={this.onUpdateNode}
+          onSelectEdge={this.onSelectEdge}
+          onCreateNode={this.onCreateNode}
+          onDeleteNode={this.onDeleteNode}
+          onSwapEdge={this.onSwapEdge}
+        />
+      </div>
+    );
+  }
+}
+
+const sample = {
+  "nodes": [
+    {
+      "id": 1,
+      "title": "A",
+      "x": 0,
+      "y": 100,
+      "type": "empty"
+    },
+    {
+      "id": 2,
+      "title": "B",
+      "x": 200,
+      "y": 100,
+      "type": "empty",
+    },
+    {
+      "id": 3,
+      "title": "C",
+      "x": 200,
+      "y": 100,
+      "type": "empty"
+    },
+    {
+      "id": 4,
+      "title": "D",
+      "x": 300,
+      "y": 200,
+      "type": "empty",
+    },
+    {
+      "id": 5,
+      "title": "E",
+      "x": 300,
+      "y": 300,
+      "type": "empty"
+    },
+    {
+      "id": 6,
+      "title": "F",
+      "x": 400,
+      "y": 300,
+      "type": "empty",
+    },
+    {
+      "id": 7,
+      "title": "G",
+      "x": 400,
+      "y": 400,
+      "type": "empty",
+    },
+    {
+      "id": 8,
+      "title": "H",
+      "x": 500,
+      "y": 400,
+      "type": "empty",
+    },
+    {
+      "id": 9,
+      "title": "I",
+      "x": 500,
+      "y": 500,
+      "type": "empty",
+    },
+  ],
+  "edges": [
+    {
+      "source": 1,
+      "target": 2,
+      "type": "edgeWithWeight",
+      "handleText": 5
+    },
+  ]
+}
